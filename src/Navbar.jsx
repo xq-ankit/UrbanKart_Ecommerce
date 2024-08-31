1f3d1305a8e88caf1df56fe1dcc0e50205de8c77
Ankit Gupta <guptankit17102004@gamil.com>
2024-08-31 14:31:46 +0530

    logout added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,16 @@
 import React,{useContext} from 'react';
 import { LiaShoppingBagSolid } from "react-icons/lia";
+import { CiLogout } from "react-icons/ci";
 import { Link } from 'react-router-dom';
 import withUser from './withUser';
 import { cartContext } from './contexts';
 
-function Navbar() {
+function Navbar({ user, setUser }) {
   const{cartcount}=useContext(cartContext);
+  const handelLogout = () => {
+    localStorage.removeItem("token");
+    setUser(undefined);
+}
   return (
     <div className="flex justify-between items-center px-4 sm:px-40 py-4 bg-white shadow-md z-50 min-w-full">
       <picture>
@@ -20,6 +25,7 @@ function Navbar() {
           alt="Logo"
         />
       </picture>
+      <button className='border rounded-full' onClick={handelLogout}><CiLogout /></button>
       <div className="relative">
      <Link to='/cart' ><LiaShoppingBagSolid className="text-3xl text-gray-800" /></Link>  
         {cartcount > 0 && (
